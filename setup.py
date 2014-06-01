8acd4c97624c89f1364d3fd510f313d92c5b652e
Michael Elsdörfer <michael@elsdoerfer.com>
2014-06-01 13:53:53 +0200

    Add setup.py file.

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+
+setup(
+    name='rippletxt',
+    url='https://github.com/miracle2k/rippletxt',
+    version='1.0',
+    license='BSD',
+    author=u'Michael Elsdörfer',
+    author_email='michael@elsdoerfer.com',
+    description=
+        'Parser for ripple.txt files',
+    py_modules=['rippletxt'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
